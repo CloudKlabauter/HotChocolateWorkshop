1b33facea0daa855701214a0724c3d7a1c76e622
roeb <info@meyer-consulting.net>
2021-11-29 12:09:44 +0100

    Add Challenge 4

---
+++ challenges/04-understanding-middleware/start/ConferencePlanner.GraphQL/Speakers/SpeakerQueries.cs
@@ -0,0 +1,25 @@
+using ConferencePlanner.GraphQL.Data;
+using Microsoft.EntityFrameworkCore;
+using ConferencePlanner.GraphQL.DataLoader;
+
+namespace ConferencePlanner.GraphQL.Speakers;
+
+[ExtendObjectType("Query")]
+public class SpeakerQueries
+{
+    [UseApplicationDbContext]
+    public Task<List<Speaker>> GetSpeakers([ScopedService] ApplicationDbContext context) =>
+        context.Speakers.ToListAsync();
+
+    public Task<Speaker> GetSpeakerByIdAsync(
+        [ID(nameof(Speaker))] int id,
+        SpeakerByIdDataLoader dataLoader,
+        CancellationToken cancellationToken) =>
+        dataLoader.LoadAsync(id, cancellationToken);
+
+    public async Task<IEnumerable<Speaker>> GetSpeakersByIdAsync(
+        [ID(nameof(Speaker))] int[] ids,
+        SpeakerByIdDataLoader dataLoader,
+        CancellationToken cancellationToken) =>
+        await dataLoader.LoadAsync(ids, cancellationToken);
+}
