6b72885f5b0509a329060da8e2d80c9e8118cab8
Thomas Freudenberg <t.freudenberg@cloudklabauter.de>
2021-12-02 08:40:00 +0100

    adds final code for challenge 7

---
+++ challenges/07-graphql-client/final/GraphQLConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using GraphQLConsole.GraphQL;
+using StrawberryShake;
+
+var serviceCollection = new ServiceCollection();
+
+serviceCollection
+    .AddConferenceClient()
+    .ConfigureHttpClient(client => client.BaseAddress = new Uri("https://workshop.chillicream.com/graphql")   );
+
+IServiceProvider services = serviceCollection.BuildServiceProvider(         );
+
+IConferenceClient client = services.GetRequiredService<IConferenceClient>();
+
+var getSessionResult = await client.GetSessions.ExecuteAsync();
+getSessionResult.EnsureNoErrors();
+
+foreach (var session in getSessionResult.Data!.Sessions!.Nodes!)
+{
+    Console.WriteLine(session.Title);
+}
+
+var addSpeakerResult = await client.AddSpeaker.ExecuteAsync("Torsten Weber", null, null);
+addSpeakerResult.EnsureNoErrors();
+
+Console.WriteLine($"Id of new speaker: {addSpeakerResult.Data!.AddSpeaker.Speaker!.Id}");
